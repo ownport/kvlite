a0e944a3b9427b33142cd35ba39f5359b4801912
Andrey Usov <ownport@gmail.com>
2012-09-07 15:54:46 +0300

    extend test case for open()

--- tests/test_kvlite_utils.py
+++ tests/test_kvlite_utils.py
@@ -2,9 +2,14 @@ import kvlite
 import unittest
 
 class KvliteUtilsTests(unittest.TestCase):
+
+    def test_kvlite_open(self):
+        print kvlite.open('sqlite://memory:test')                
+        print kvlite.open('sqlite://tests/db/test.sqlite:test')                
+        # print kvlite.open('mysql://memory:test')                
         
     def test_parse_uri_sqlite(self):
-        print kvlite.parse_uri('sqlite://')
+        pass
 
         
 if __name__ == '__main__':
