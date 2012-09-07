f918bc2195045c6d1a423cb469e70f14903aaf21
Andrey Usov <ownport@gmail.com>
2012-09-07 10:30:17 +0300

    added unittests for kvlite utils

---
+++ tests/test_kvlite_utils.py
@@ -0,0 +1,12 @@
+import kvlite
+import unittest
+
+class KvliteUtilsTests(unittest.TestCase):
+        
+    def test_parse_uri_sqlite(self):
+        pass
+
+        
+if __name__ == '__main__':
+    unittest.main()        
+
