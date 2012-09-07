f8d1d5f9ca3055d41cf860942fbc3343abf0358b
Andrey Usov <ownport@gmail.com>
2012-09-07 18:16:17 +0300

    update test cases for open collection

--- tests/test_kvlite_utils.py
+++ tests/test_kvlite_utils.py
@@ -6,7 +6,7 @@ class KvliteUtilsTests(unittest.TestCase):
     def test_kvlite_open(self):
         print kvlite.open('sqlite://memory:test')                
         print kvlite.open('sqlite://tests/db/test.sqlite:test')                
-        # print kvlite.open('mysql://memory:test')                
+        print kvlite.open('mysql://kvlite_test:eixaaghiequ6ZeiBahn0@localhost/kvlite_test.test_collection')                
         
     def test_parse_uri_sqlite(self):
         pass
