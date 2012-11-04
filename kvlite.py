8ab6e72d85dfcc2c9a7719048da2a7c27d8bd000
ownport <ownport@gmail.com>
2012-11-04 10:08:28 +0200

    changed __version__ to 0.5

--- kvlite.py
+++ kvlite.py
@@ -10,7 +10,7 @@
 #   kvlite2 tutorial http://code.google.com/p/kvlite/wiki/kvlite2
 #
 __author__ = 'Andrey Usov <https://github.com/ownport/kvlite>'
-__version__ = '0.4.5'
+__version__ = '0.5'
 __license__ = """
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
