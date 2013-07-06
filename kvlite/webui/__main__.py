8e6f79b9cfd06ff90aea133118899af4ba6510fb
ownport <ownport@gmail.com>
2013-07-06 21:35:11 +0300

    add test actions for webui

--- kvlite/webui/__main__.py
+++ kvlite/webui/__main__.py
@@ -3,24 +3,35 @@ import bottle
 
 from kvlite.webui import settings
 
-# favicon.ico handling
-def handle_favicon():
-    return bottle.static_file('images/favicon.ico', root=settings.STATIC_PATH)
-
-# static files handling
-def handle_static(filepath):
-    return bottle.static_file(filepath, root=settings.STATIC_PATH)
-
-    
 @bottle.route('/')
 def index():
-    return bottle.static_file('index.html', root='static/html/')
-
-@bottle.route('/static/<filename>')
-def static_files(filename):
-    ''' handle static files
+    ''' handle index.html
     '''
-    return bottle.static_file(filename, root='static/')
+    return bottle.static_file('html/index.html', root=settings.STATIC_PATH)
+
+@bottle.route('/favicon.ico')
+def index():
+    ''' handle favicon.ico
+    '''
+    return bottle.static_file('img/favicon.ico', root=settings.STATIC_PATH)
+
+@bottle.route('/img/<filename>')
+def static_img_files(filename):
+    ''' handle image files
+    '''
+    return bottle.static_file(filename, root=os.path.join(settings.STATIC_PATH, 'img'))
+
+@bottle.route('/css/<filename>')
+def static_css_files(filename):
+    ''' handle css files
+    '''
+    return bottle.static_file(filename, root=os.path.join(settings.STATIC_PATH, 'css'))
+
+@bottle.route('/js/<filename>')
+def static_js_files(filename):
+    ''' handle JavaScript files
+    '''
+    return bottle.static_file(filename, root=os.path.join(settings.STATIC_PATH, 'js'))
 
 if __name__ == '__main__':
     
