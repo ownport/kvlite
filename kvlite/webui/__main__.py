4c49d05cfa2560e4eb0062eb676c2621810eb0d2
ownport <ownport@gmail.com>
2013-07-10 21:28:31 +0300

    frontend re-factoring

--- kvlite/webui/__main__.py
+++ kvlite/webui/__main__.py
@@ -1,8 +1,12 @@
 import os
 import bottle
+import kvlite
 
 from kvlite.webui import settings
 
+#
+#   Service commands
+#
 @bottle.route('/')
 def index():
     ''' handle index.html
@@ -10,28 +14,48 @@ def index():
     return bottle.static_file('html/index.html', root=settings.STATIC_PATH)
 
 @bottle.route('/favicon.ico')
-def index():
+def get_favicon():
     ''' handle favicon.ico
     '''
     return bottle.static_file('img/favicon.ico', root=settings.STATIC_PATH)
 
 @bottle.route('/img/<filename>')
-def static_img_files(filename):
+def get_static_img_files(filename):
     ''' handle image files
     '''
     return bottle.static_file(filename, root=os.path.join(settings.STATIC_PATH, 'img'))
 
 @bottle.route('/css/<filename>')
-def static_css_files(filename):
+def get_static_css_files(filename):
     ''' handle css files
     '''
     return bottle.static_file(filename, root=os.path.join(settings.STATIC_PATH, 'css'))
 
 @bottle.route('/js/<filename>')
-def static_js_files(filename):
+def get_static_js_files(filename):
     ''' handle JavaScript files
     '''
     return bottle.static_file(filename, root=os.path.join(settings.STATIC_PATH, 'js'))
+
+#
+#   kvlite's webui api
+#
+@bottle.route('/collections')
+def collections_list():
+    ''' returns the list of collections
+    '''
+    return {'collections': settings.COLLECTIONS.keys() }
+
+@bottle.route('/collection/<name>/')
+def get(name):
+    ''' return collection data 
+    '''
+    try:
+        collection_uri = settings.COLLECTIONS[name]
+    except KeyError:
+        return {'error': 'The collection %s is not found' % name, 'status': 'NOT OK'}
+    collection = kvlite.open(collection_uri)
+    return {'error': '', 'status': 'OK', 'data': [kv for kv in collection.get()]}
 
 if __name__ == '__main__':
     
